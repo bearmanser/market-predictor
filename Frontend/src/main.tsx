2d58da682657de89342709c610167706157315d3
Odd Magnus Grinder <omgrinder10@gmail.com>
2026-03-14 16:24:28 +0100

    Init React

---
+++ Frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+const system = createSystem(defaultConfig);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
